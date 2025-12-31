d3efced6a7e956174a426a211e9a20b0fa7a2f74
seeron6 <49841737+seeron6@users.noreply.github.com>
2025-12-31 01:03:04 -0500

    deleted index.css

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,26 +1,37 @@
-declare module "*.png" {
+declare module '*.svg' {
+    import * as React from 'react';
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+    const src: string;
+    export default src;
+  }
+  
+  declare module '*.png' {
     const value: string;
     export default value;
   }
   
-  declare module "*.jpg" {
+  declare module '*.jpg' {
     const value: string;
     export default value;
   }
   
-  declare module "*.jpeg" {
+  declare module '*.jpeg' {
     const value: string;
     export default value;
   }
   
-  declare module "*.svg" {
+  declare module '*.gif' {
     const value: string;
     export default value;
   }
   
-  declare module "*.gif" {
+  declare module '*.webp' {
     const value: string;
     export default value;
   }
-
+  
+  declare module '*.bmp' {
+    const value: string;
+    export default value;
+  }
   
