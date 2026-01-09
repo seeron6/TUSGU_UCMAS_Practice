43c9d185fa66fe1dc9e2b13257f92b13b05f9b18
seeron6 <49841737+seeron6@users.noreply.github.com>
2026-01-08 21:29:52 -0500

    App photo change in home screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tusgu.education',
-  appName: 'TUSGU & UCMAS Sri Lanka',
+  appName: 'UCMAS Sri Lanka & TUSGU',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
