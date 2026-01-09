1ec229d47e4d32ca97c1401c5b2fb52d3a031d33
seeron6 <49841737+seeron6@users.noreply.github.com>
2026-01-08 19:37:47 -0500

    naming changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tusgu.education',
-  appName: 'TUSGU Portal',
+  appName: 'TUSGU & UCMAS Sri Lanka',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
